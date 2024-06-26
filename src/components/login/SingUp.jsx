f84e0154322142592b0c5f16c5f6e31658d4ba1c
DANIEL DOUGLAS DOS SANTOS <daniel04.santos@catolicasc.edu.br>
2024-06-26 20:50:00 -0300

    bug cadastro

--- src/components/login/SingUp.jsx
+++ src/components/login/SingUp.jsx
@@ -10,60 +10,48 @@ const SingUp = ({
   passwordConfirm,
   handleSubmit,
 }) => {
-  const [modalOpen, setModalOpen] = useState(true);
-
-  const handleCloseModal = () => {
-    setModalOpen(false);
-  };
-
-  const handleFormSubmit = (e) => {
-    handleSubmit(e);
-
-    setModalOpen(false);
-  };
   return (
     <>
-      {modalOpen && (
-        <form onSubmit={handleFormSubmit}>
-          <Stack direction="column" spacing={1} sx={{ pb: 2 }}>
-            <Stack direction="row" spacing={1}>
-              <Typography id="modal-modal-title" variant="h6">
-                Cadastrar novo usuário
-              </Typography>
-            </Stack>
-
-            <TextField
-              id="outlined-basic"
-              label="Nome de usuario"
-              variant="outlined"
-              value={user}
-              onChange={(e) => setUser(e.target.value)}
-              required
-            />
-            <TextField
-              type="password"
-              id="outlined-basic"
-              label="Senha"
-              variant="outlined"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-            <TextField
-              type="password"
-              id="outlined-basic"
-              label="Confirmar senha"
-              variant="outlined"
-              value={passwordConfirm}
-              onChange={(e) => setPasswordConfirm(e.target.value)}
-              required
-            />
-            <Button variant="contained" type="submit">
-              Cadastrar
-            </Button>
+      {" "}
+      <form onSubmit={handleSubmit}>
+        <Stack direction="column" spacing={1} sx={{ pb: 2 }}>
+          <Stack direction="row" spacing={1}>
+            <Typography id="modal-modal-title" variant="h6">
+              Cadastrar novo usuário
+            </Typography>
           </Stack>
-        </form>
-      )}
+
+          <TextField
+            id="outlined-basic"
+            label="Nome de usuario"
+            variant="outlined"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+            required
+          />
+          <TextField
+            type="password"
+            id="outlined-basic"
+            label="Senha"
+            variant="outlined"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <TextField
+            type="password"
+            id="outlined-basic"
+            label="Confirmar senha"
+            variant="outlined"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+            required
+          />
+          <Button variant="contained" type="submit">
+            Cadastrar
+          </Button>
+        </Stack>
+      </form>
       <hr />
     </>
   );
